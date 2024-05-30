e6babb10e64b9b7002bb6682d80a33858d0a2ce8
Lewis <lewisday080@gmail.com>
2024-05-30 15:10:06 +0100

    last big batch
    it is impending

---
+++ Assets/AAAGLDBRM2023/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,67 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID BIG_DEMON_DEAD = 2472533742U;
+        static const AkUniqueID BIG_DEMON_INJURED = 2180471771U;
+        static const AkUniqueID BIG_DEMON_MELEE = 1479837664U;
+        static const AkUniqueID BUTTON_CLICK = 814543256U;
+        static const AkUniqueID CASTLE_THEMES = 305072610U;
+        static const AkUniqueID DAGGER_SLASH = 114434101U;
+        static const AkUniqueID DUNGEON_MUSIC = 1070605181U;
+        static const AkUniqueID MAGIC_PROJECTILE = 4103199500U;
+        static const AkUniqueID MENU_END = 2635200406U;
+        static const AkUniqueID MENU_START = 3908297853U;
+        static const AkUniqueID PLAY_BURN_TEST = 2393980604U;
+        static const AkUniqueID PLAY_GAMEOVER_CUE = 540387002U;
+        static const AkUniqueID PLAY_TORCHES = 1256842828U;
+        static const AkUniqueID PLAYER_INJURED = 27255824U;
+        static const AkUniqueID PLAYER_LOWHEALTH = 3634256599U;
+        static const AkUniqueID PLAYER_NORMALHEALTH = 2696132222U;
+        static const AkUniqueID SHOTGUN_FIRE = 3982195600U;
+        static const AkUniqueID SLUMS_END = 2022975615U;
+        static const AkUniqueID SLUMS_THEME = 208989199U;
+        static const AkUniqueID SMALL_DEMON_DEAD = 2182621371U;
+        static const AkUniqueID SMALL_DEMON_FIREBALL = 658204658U;
+        static const AkUniqueID SMALL_DEMON_INJURED = 1777701508U;
+        static const AkUniqueID SMALL_DEMON_MELEE = 3767552163U;
+        static const AkUniqueID WARDEN_DEAD = 2607442107U;
+        static const AkUniqueID WARDEN_DRILLHANDS = 145123698U;
+        static const AkUniqueID WARDEN_INJURED = 3620622980U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID DUNGEON_SOUNDBANK = 2508231523U;
+        static const AkUniqueID MAIN = 3161908922U;
+        static const AkUniqueID MAIN_MENU_MUSIC = 3399698792U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID MUSIC_SLUMS = 1368239717U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
